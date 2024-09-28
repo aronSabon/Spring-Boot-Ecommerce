c6e17f743a46a5fc4186e8ac19ba47c3267533d2
Lemon <sabinbasi111@gmail.com>
2024-09-28 12:59:34 +0545

    api for angular

--- src/main/java/com/broadway/springbootEMS/api/EmployeeRestController.java
+++ src/main/java/com/broadway/springbootEMS/api/EmployeeRestController.java
@@ -1,8 +1,13 @@
 package com.broadway.springbootEMS.api;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,57 +18,77 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.broadway.springbootEMS.model.Employee;
+import com.broadway.springbootEMS.model.Employee;
 import com.broadway.springbootEMS.model.Product;
 import com.broadway.springbootEMS.repository.ProductRepository;
 import com.broadway.springbootEMS.service.EmployeeService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200") // Allow requests from your Angular app
+
 public class EmployeeRestController {
 	
 	@Autowired   
 	private EmployeeService empService;  
-	@Autowired
-	private ProductRepository pr;
 	
-	@GetMapping("/api/emp/list")
+	@PostMapping("/api/employee/add")
+	//	public String add(@RequestBody Employee dept) {
+	public ResponseEntity<Map<String, Object>> add(@RequestBody Employee emp) {
+		empService.addEmployee(emp);
+		Map<String, Object> response = new HashMap<>();
+		response.put("success", true);
+		return ResponseEntity.ok(response);
+		//		return "success";
+	}
+	@GetMapping("/api/employee/list")
 	public List<Employee> getAll() {
 		return empService.getAllEmployee();
 	}
+	
+	
+	
+	@Autowired
+	private ProductRepository pr;
+//	
+//	@GetMapping("/api/emp/list")
+//	public List<Employee> getAll() {
+//		return empService.getAllEmployee();
+//	}
 	@GetMapping("/api/emp/{id}")
 	public Employee getOneEmp(@PathVariable int id) {
 		return empService.getEmployeeById(id);
 	}
-	@PostMapping("/api/emp/add")
-	public String add(@RequestBody Employee emp) {
+	@PostMapping("/api/employee/addd")
+	public String addd(@RequestBody Employee emp) {
 		empService.addEmployee(emp);
 		return "success";
 	}
-	@DeleteMapping("/api/emp/delete/{id}")
-	public String delete(@PathVariable int id) {
-		empService.deleteEmployeeById(id);
-		return "success";
-	}
-	@PutMapping("/api/emp/update")
-	public String update(@RequestBody Employee emp) {
-		empService.updateEmployee(emp);
-		return "Success";
-	}
-	@GetMapping("/api/emp/j2o")
-	public String jsonToObject() {
-		RestTemplate rest= new RestTemplate();
-		Employee emp =rest.getForObject("http://localhost:8080/api/emp/6", Employee.class);
-		
-//		return"FirstName= "+ emp.getFirstName();
-		return null;
-	}
-	@GetMapping("/api/emp/ja2oa")
-	public String jsonArrayToObjArray() {
-		RestTemplate rest = new RestTemplate();
-		Employee[] emps = rest.getForObject("http://localhost:8080/api/emp/list", Employee[].class);
-//		return "Name= "+emps[0].getFirstName()+" "+emps[0].getLastName();
-		return null;
-	}
-	//     localhost:8080/api/emp/pload        use this url when using the application for first time
+//	@DeleteMapping("/api/emp/delete/{id}")
+//	public String delete(@PathVariable int id) {
+//		empService.deleteEmployeeById(id);
+//		return "success";
+//	}
+//	@PutMapping("/api/emp/update")
+//	public String update(@RequestBody Employee emp) {
+//		empService.updateEmployee(emp);
+//		return "Success";
+//	}
+//	@GetMapping("/api/emp/j2o")
+//	public String jsonToObject() {
+//		RestTemplate rest= new RestTemplate();
+//		Employee emp =rest.getForObject("http://localhost:8080/api/emp/6", Employee.class);
+//		
+////		return"FirstName= "+ emp.getFirstName();
+//		return null;
+//	}
+//	@GetMapping("/api/emp/ja2oa")
+//	public String jsonArrayToObjArray() {
+//		RestTemplate rest = new RestTemplate();
+//		Employee[] emps = rest.getForObject("http://localhost:8080/api/emp/list", Employee[].class);
+////		return "Name= "+emps[0].getFirstName()+" "+emps[0].getLastName();
+//		return null;
+//	}
+//	//     localhost:8080/api/emp/pload        use this url when using the application for first time
 	@GetMapping("/api/emp/pload")
 	public String loadProducts() {
 		RestTemplate rest= new RestTemplate();
